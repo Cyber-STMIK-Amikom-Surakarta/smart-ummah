0bcd69d819170635771d054f75c8c8f13300e25a
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-01-02 11:26:22 +0700

    📦 add blog component

---
+++ src/dummy/Blog.js
@@ -0,0 +1,29 @@
+import Blog1Image from '../assets/images/blog/blog1.jpg';
+import Blog2Image from '../assets/images/blog/blog2.jpg';
+import Blog3Image from '../assets/images/blog/blog3.jpg';
+
+const articles = [
+  {
+    img: Blog1Image,
+    title: '6 Strategies to Find Your Conference Keynote and Other Speakers',
+    body: 'Sekarang, kamu bisa produksi tiket fisik untuk eventmu bersama Bostiketbos. Hanya perlu mengikuti beberapa langkah mudah.',
+    date: '12 Mar',
+    author: 'Jhon Doe',
+  },
+  {
+    img: Blog2Image,
+    title: 'How Successfully Used Paid Marketing to Drive Incremental Ticket Sales',
+    body: 'Sekarang, kamu bisa produksi tiket fisik untuk eventmu bersama Bostiketbos. Hanya perlu mengikuti beberapa langkah mudah.',
+    date: '12 Mar',
+    author: 'Jhon Doe',
+  },
+  {
+    img: Blog3Image,
+    title: 'Introducing Workspaces: Work smarter, not harder with new navigation',
+    body: 'Sekarang, kamu bisa produksi tiket fisik untuk eventmu bersama Bostiketbos. Hanya perlu mengikuti beberapa langkah mudah.',
+    date: '12 Mar',
+    author: 'Jhon Doe',
+  },
+]
+
+export default articles;
