febab4cb8d05a5a16acb0e7ab840fafd813b925c
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-01-04 06:50:06 +0700

    📦 Implement e-learning

---
+++ src/shared/Tag.jsx
@@ -0,0 +1,7 @@
+export default function Tag({ children }) {
+  return (
+    <button className='text-white border-[#F1F1F3] border-[1px] bg-[#8ADAB2] rounded-[30px] py-[8px] px-[8px] font-semibold'>
+      {children}
+    </button>
+  )
+}
