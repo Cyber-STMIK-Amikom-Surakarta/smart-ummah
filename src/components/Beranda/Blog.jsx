63dc3751a86fd0d8ed5f74f3c1a0b8b751e5140d
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-01-07 11:23:17 +0700

    🛠️ Revisi blog component

--- src/components/Beranda/Blog.jsx
+++ src/components/Beranda/Blog.jsx
@@ -1,12 +1,12 @@
 function Article({ img, title, body, author, date }) {
   return (
-    <div className="flex flex-col gap-1">
-      <div className="flex flex-col gap-1 justify-center items-center">
-        <img className="object-cover rounded sm:w-[95vw] md:w-[calc(100vw/3)] h-[300px]" src={img} alt={title} />
-        <h2 className="text-xl text-[#242565]">{title}</h2>
+    <div className="relative flex flex-col gap-1">
+      <div className="relative flex flex-col gap-1 justify-center">
+        <img className="object-cover object-top rounded sm:w-[95vw] h-[300px]" src={img} alt={title} />
+        <h2 className="text-left text-xl text-[#242565]">{title}</h2>
         <p>{body}</p>
       </div>
-      <span className="text-left text-[#A5A5A5]">{date} - {author}</span>
+      <span className="text-left text-[#5D5A88] font-bold">{date} - {author}</span>
     </div>
   )
 }
@@ -19,8 +19,8 @@ export default function Blog({ articles }) {
 
   return (
     <section className="flex flex-col gap-4 justify-center items-center">
-      <h1 className="text-5xl text-[#5D5A88]">Blog</h1>
-      <p className="text-[#6A6A6A]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+      <h1 className="text-5xl font-bold text-[#5D5A88]">Blog</h1>
+      <p className="text-[#6A6A6A] text-lg text-center w-[70%] font-semibold">membahas langkah-langkah praktis menuju kebaikan dan bagaimana Islam memberikan pedoman untuk mencapai ketenangan batin.</p>
       <div className="flex sm:flex-col md:flex-row gap-4 justify-center items-center">
         {Articles}
       </div>
--- src/dummy/Blog.js
+++ src/dummy/Blog.js
@@ -1,28 +1,28 @@
 import Blog1Image from '../assets/images/blog/blog1.jpg';
-import Blog2Image from '../assets/images/blog/blog2.jpg';
-import Blog3Image from '../assets/images/blog/blog3.jpg';
+import Blog2Image from '../assets/images/blog/blog1.jpg';
+import Blog3Image from '../assets/images/blog/blog1.jpg';
 
 const articles = [
   {
     img: Blog1Image,
-    title: '6 Strategies to Find Your Conference Keynote and Other Speakers',
-    body: 'Sekarang, kamu bisa produksi tiket fisik untuk eventmu bersama Bostiketbos. Hanya perlu mengikuti beberapa langkah mudah.',
+    title: 'Membangun Kebiasaan Spiritual Harian',
+    body: 'Dari waktu sholat hingga membaca Al-Qur\'an, kita akan membahas bagaimana merintis kehidupan spiritual yang konsisten membawa dampak positif dalam hidup sehari-hari.',
     date: '12 Mar',
-    author: 'Jhon Doe',
+    author: 'Anggi S',
   },
   {
     img: Blog2Image,
-    title: 'How Successfully Used Paid Marketing to Drive Incremental Ticket Sales',
-    body: 'Sekarang, kamu bisa produksi tiket fisik untuk eventmu bersama Bostiketbos. Hanya perlu mengikuti beberapa langkah mudah.',
+    title: 'Membangun Kebiasaan Spiritual Harian',
+    body: 'Dari waktu sholat hingga membaca Al-Qur\'an, kita akan membahas bagaimana merintis kehidupan spiritual yang konsisten membawa dampak positif dalam hidup sehari-hari.',
     date: '12 Mar',
-    author: 'Jhon Doe',
+    author: 'Anggi S',
   },
   {
     img: Blog3Image,
-    title: 'Introducing Workspaces: Work smarter, not harder with new navigation',
-    body: 'Sekarang, kamu bisa produksi tiket fisik untuk eventmu bersama Bostiketbos. Hanya perlu mengikuti beberapa langkah mudah.',
+    title: 'Membangun Kebiasaan Spiritual Harian',
+    body: 'Dari waktu sholat hingga membaca Al-Qur\'an, kita akan membahas bagaimana merintis kehidupan spiritual yang konsisten membawa dampak positif dalam hidup sehari-hari.',
     date: '12 Mar',
-    author: 'Jhon Doe',
+    author: 'Anggi S',
   },
 ]
 
