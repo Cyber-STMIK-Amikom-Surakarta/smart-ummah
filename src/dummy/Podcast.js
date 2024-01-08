29e1ebc5253b7256472bfb4b492114a7cdb8b4a1
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-01-08 07:07:46 +0700

    📦 implementasi podcast

---
+++ src/dummy/Podcast.js
@@ -0,0 +1,51 @@
+import PodcastImg1 from '../assets/images/podcast/1.jpg'
+import PodcastImg2 from '../assets/images/podcast/2.jpg'
+import PodcastImg3 from '../assets/images/podcast/3.jpg'
+import PodcastImg4 from '../assets/images/podcast/4.jpg'
+import PodcastImg5 from '../assets/images/podcast/5.jpg'
+import PodcastImg6 from '../assets/images/podcast/6.jpg'
+import PodcastImg7 from '../assets/images/podcast/7.jpg'
+import PodcastImg8 from '../assets/images/podcast/8.jpg'
+
+export default [
+  {
+    img: PodcastImg1,
+    title: 'Tetesan Kehidupan',
+    author: 'Ustadz Luqman Hakim',
+  },
+  {
+    img: PodcastImg2,
+    title: 'Tetesan Kehidupan',
+    author: 'Ustadz Luqman Hakim',
+  },
+  {
+    img: PodcastImg3,
+    title: 'Tetesan Kehidupan',
+    author: 'Ustadz Luqman Hakim',
+  },
+  {
+    img: PodcastImg4,
+    title: 'Tetesan Kehidupan',
+    author: 'Ustadz Luqman Hakim',
+  },
+  {
+    img: PodcastImg5,
+    title: 'Tetesan Kehidupan',
+    author: 'Ustadz Luqman Hakim',
+  },
+  {
+    img: PodcastImg6,
+    title: 'Tetesan Kehidupan',
+    author: 'Ustadz Luqman Hakim',
+  },
+  {
+    img: PodcastImg7,
+    title: 'Tetesan Kehidupan',
+    author: 'Ustadz Luqman Hakim',
+  },
+  {
+    img: PodcastImg8,
+    title: 'Tetesan Kehidupan',
+    author: 'Ustadz Luqman Hakim',
+  },
+]
